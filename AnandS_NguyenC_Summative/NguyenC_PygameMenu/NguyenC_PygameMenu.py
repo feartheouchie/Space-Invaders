f2b523e1814b8fc22b0fc5cf78a9a213dff61ff5
pottedplant42069 <candywebkin@gmail.com>
2017-06-12 10:06:18 -0400

    i tried 2.0

--- AnandS_NguyenC_Summative/NguyenC_PygameMenu/NguyenC_PygameMenu.py
+++ AnandS_NguyenC_Summative/NguyenC_PygameMenu/NguyenC_PygameMenu.py
@@ -29,13 +29,40 @@ font_path = "PixelSplitter-Bold.ttf"
 font_size = 10
 fontObj = pygame.font.Font(font_path, font_size)
 
+#objects
 spaceship=pygame.image.load("spaceship1.png").convert_alpha()
+asteroid=pygame.image.load("newast.png").convert_alpha()
+ufo=pygame.image.load("UFO.png").convert_alpha()
+
+
+#sound
 mute=pygame.image.load("volume\mute.png").convert_alpha()
 sound=pygame.image.load("volume\sound.png").convert_alpha()
 mute2=pygame.image.load("volume\mute2.png").convert_alpha()
 sound2=pygame.image.load("volume\sound2.png").convert_alpha()
 
+#text
 credit = fontObj.render(('Credits'), True, (255,255,255))
+credittext1 = fontObj.render(('Graphic Designer'), True, (255,255,255))
+credittext2 = fontObj.render(('Software Engineer'), True, (255,255,255))
+musictext = fontObj.render(('Music'), True, (255,255,255))
+
+instructionstext1 = fontObj.render(('Use arrow keys to move your spaceship'), True, (255,255,255))
+instructionstext2 = fontObj.render(('Press spacebar to shoot'), True, (255,255,255))
+instructionstext3 = fontObj.render(('Dodge lasers by hiding behind the asteroids'), True, (255,255,255))
+
+hp=fontObj.render(('12 HP'), True, (0,245,46))
+orange=fontObj.render(('10 pts'), True, (0,245,46))
+blue=fontObj.render(('20 pts'), True, (0,245,46))
+green=fontObj.render(('40 pts'), True, (0,245,46))
+ufotext=fontObj.render(('??? pts'), True, (0,245,46))
+
+#enemies
+orangeenemy=pygame.image.load("enemy1.png").convert_alpha()
+blueenemy=pygame.image.load("enemy2.png").convert_alpha()
+greenenemy=pygame.image.load("enemy3.png").convert_alpha()
+
+#buttons
 btnStart=pygame.image.load("start\startbutt1.png").convert_alpha()
 btnInstruct=pygame.image.load("instru\instrubutt1.png").convert_alpha()
 btnExit=pygame.image.load("exit\exit1.png").convert_alpha()
@@ -47,24 +74,23 @@ btnInstruct2=pygame.image.load("instru\instrubutt2.png").convert_alpha()
 btnExit2=pygame.image.load("exit\exit2.png").convert_alpha()
 btnBack2=pygame.image.load("back\\back2.png").convert_alpha()
 
+#variables
 state="menu"
 volume="sound"
 music = "start"
+
+#blit sound
 m=screen.blit(mute, (600, 450))
 s=screen.blit(sound, (600, 450))
 
+#music
 pygame.mixer.music.load("DANCE_TILL_YOURE_DEAD.wav")
-
-
-
-
 
 try:
     while keepGoing:
         clock.tick(60)
         screen.blit(background, (0, 0))
         
-
         x=265
         y=200
         if state=="menu":
@@ -73,7 +99,6 @@ try:
 
             if volume=="mute":
                 m=screen.blit(mute, (600, 450))
-
                 
             a=pygame.mouse.get_pos()
         
@@ -148,10 +173,6 @@ try:
                             else:
                                 volume="sound"
 
-
-
-                
-            
         if state=="game":  
             # ---------------code for the game-------------------               
             a=pygame.mouse.get_pos()
@@ -177,14 +198,74 @@ try:
         if state=="instructions":
             a=pygame.mouse.get_pos()
 
+        
             screen.blit(background3, (0, 0))
+
+            if volume=="sound":
+                s=screen.blit(sound, (600, 450))
+
+            if volume=="mute":
+                m=screen.blit(mute, (600, 450))
+                
             bb=screen.blit(btnBack1,(x,y+200))
-##            screen.blit(instrutext, (x-100, 0))
+
+            screen.blit(instructionstext1, (25,125))
+            screen.blit(instructionstext2, (25,150))
+            screen.blit(instructionstext3, (25,175))
+            
+            screen.blit(orangeenemy, (425,250))
+            screen.blit(blueenemy, (375,250))
+            screen.blit(greenenemy, (325,250))
+
+            screen.blit(orange, (425,275))
+            screen.blit(blue, (375,275))
+            screen.blit(green, (325,275))
+            screen.blit(ufotext, (175,275))
+
+            screen.blit(spaceship, (350, 100))
+            screen.blit(asteroid, (25, 250))
+            screen.blit(ufo, (175, 250))
+
+            screen.blit(hp, (25, 250))
 
 
             if bb.collidepoint(a):
                 screen.blit(background3, (0, 0))
+                screen.blit(instructionstext1, (25,125))
+                screen.blit(instructionstext2, (25,150))
+                screen.blit(instructionstext3, (25,175))
+                screen.blit(orangeenemy, (425,250))
+                screen.blit(blueenemy, (375,250))
+                screen.blit(greenenemy, (325,250))
+
+                screen.blit(orange, (425,275))
+                screen.blit(blue, (375,275))
+                screen.blit(green, (325,275))
+                screen.blit(ufotext, (175,275))
+
+                screen.blit(spaceship, (350, 100))
+                screen.blit(asteroid, (25, 250))
+                screen.blit(ufo, (175, 250))
+
+                screen.blit(hp, (25, 250)) 
                 bb=screen.blit(btnBack2, (x, y+200))
+
+            if s.collidepoint(a) and volume!="mute":
+                screen.blit(sound2, (600, 450))
+
+            if music == "start" and volume == "sound":
+                pygame.mixer.music.play(-1)
+                music = "playing"
+            elif volume == "mute":
+                pygame.mixer.music.pause()
+                music = "stopped"
+                if m.collidepoint(a):
+                    screen.blit(mute2, (600, 450))
+            elif music == "stopped" and volume == "sound":
+                pygame.mixer.music.unpause()
+                music = "playing"
+                if s.collidepoint(a):
+                    screen.blit(sound2, (600, 450))
             
             for ev in pygame.event.get():
                 if ev.type == pygame.QUIT:
@@ -192,11 +273,21 @@ try:
                 elif ev.type == MOUSEBUTTONDOWN:
                     if bb.collidepoint(a):
                         state="menu"
+                    else:
+                         if m.collidepoint(a) or s.collidepoint(a):
+                            if volume=="sound":
+                                volume="mute"
+                            else:
+                                volume="sound"
+
 
         if state=="credits":
             a=pygame.mouse.get_pos()
 
             screen.blit(background3, (0, 0))
+            screen.blit(credittext1, (25,125))
+            screen.blit(credittext2, (25,150))
+            screen.blit(musictext, (25,175))
             bb=screen.blit(btnBack1,(x,y+200))
 
 
